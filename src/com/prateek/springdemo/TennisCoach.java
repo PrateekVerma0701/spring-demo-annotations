1292e4733963465a37f2677650cd4b9e877a27bf
Prateek <prateek.verma0701@gmail.com>
2020-03-31 02:55:46 +0530

    Autowire - setter injection implememntation

--- src/com/prateek/springdemo/TennisCoach.java
+++ src/com/prateek/springdemo/TennisCoach.java
@@ -8,8 +8,21 @@ public class TennisCoach implements Coach {
 
 	private FortuneService fortuneService;
 
+	// define a default constructor, though it is not required.
+
+	public TennisCoach() {
+		System.out.println("Tennis Coach :  inside default constructor");
+	}
+
+	/*
+	 * @Autowired public TennisCoach(FortuneService fortuneService) {
+	 * this.fortuneService = fortuneService; }
+	 */
+
+	// define a setter method
 	@Autowired
-	public TennisCoach(FortuneService fortuneService) {
+	public void setFortuneService(FortuneService fortuneService) {
+		System.out.println("Tennis Coach :  inside setFortuneService() method");
 		this.fortuneService = fortuneService;
 	}
 
