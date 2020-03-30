c9d8d295affc53de1247d1d49237d34705c0f2dd
Prateek <prateek.verma0701@gmail.com>
2020-03-31 02:43:23 +0530

    Autowire - constructor injection

--- src/com/prateek/springdemo/TennisCoach.java
+++ src/com/prateek/springdemo/TennisCoach.java
@@ -6,8 +6,12 @@ import org.springframework.stereotype.Component;
 @Component
 public class TennisCoach implements Coach {
 
-	@Autowired
 	private FortuneService fortuneService;
+
+	@Autowired
+	public TennisCoach(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
 
 	@Override
 	public String getDailyWorkout() {
